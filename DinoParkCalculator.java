3e77e0971416d2bd9b9ad912ff94ec31347e13bc
Eric <ericyoung111@gmail.com>
2022-05-30 11:43:36 -0600

    made the choice selector and created the variables that will be used to calculate price and determine when to exit the menu

--- DinoParkCalculator.java
+++ DinoParkCalculator.java
@@ -2,7 +2,26 @@ import java.util.Scanner;
 
 public class DinoParkCalculator {
     public static void main(String[] args) {
-        System.out.println("Program is running");
+        // Create new instance of keyboard scanner
+        Scanner keyboard = new Scanner(System.in);
+        
+        // Create variables that will be used to calculate cost
+        boolean member = false;
+        int additionalAdults = 0;
+        int additionalChildren = 0;
+        int selectionValue = 0;
+
+        // Display options to user
+        System.out.println("Welcome to the Dinosaur Park Party Planner");
+        System.out.println("Choose the type of party");
+        System.out.println("1. Group Rate Admission Party");
+        System.out.println("2. Bare Bones Package");
+        System.out.println("3. Deluxe Party Package");
+        System.out.println("4. Quit");
+        System.out.println("Enter choice: ");
+        keyboard.nextInt();
+
+        keyboard.close();
     }
 }
 
